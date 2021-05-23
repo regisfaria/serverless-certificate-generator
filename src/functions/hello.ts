387e05455724410cec84721237f68e25dbe8b711
regisfaria <rgisfaria@yahoo.com.br>
2021-05-22 22:30:33 -0300

    removed example file

--- src/functions/hello.ts
+++
@@ -1,9 +0,0 @@
-export const handle = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello World' }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
-};
